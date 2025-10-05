78745ea350e4c11b23c0c99b078b9588497a79c3
NeuraGo <juansabarofek@gmail.com>
2025-10-05 12:20:39 +0700

    Portofolio 1 dari nol

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// gsap.to(".box", {
+//         repeat: -1,
+//         opacity: 0,
+//         duration: 3,
+//         ease: "power1.inOut"
+//         });
+
+gsap.to(".juan", {
+  duration: 2,
+  rotationX: 45,
+  scaleX: 0.8,
+  z: -300,
+});
+
+gsap.to('.box', {
+	scrollTrigger: '.box', // start the animation when ".box" enters the viewport (once)
+	x: -10 0.
+});
+

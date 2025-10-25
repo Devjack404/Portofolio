01256ba6b036bbd3981a4aae1885c4cb4577ffe6
Devjack404 <juansabarofek@gmail.com>
2025-10-25 11:10:47 +0700

    update index

--- js/script.js
+++ js/script.js
@@ -1,13 +1,2 @@
-gsap.to(".box", {
-        repeat: -1 ,
-        opacity: 0,
-        duration: 3,
-        ease: "power1.inOut"
-        });
 
-// gsap.to(".juan", {
-//   duration: 2,
-//   rotationX: 45,
-//   scaleX: 0.8,
-//   z: -300,
-// }
+get
